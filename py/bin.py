f33e89651fe7c0a92f8ba3089ac28aa651d7e0dd
aucub <78630225+aucub@users.noreply.github.com>
2023-11-21 23:12:54 +0800

    update check

--- py/bin.py
+++ py/bin.py
@@ -3,8 +3,8 @@ import re
 import requests
 import hashlib
 
-UPDATE = 1
-PKGREL = 0
+update = 1
+pkgrel = 0
 
 # 获取环境变量
 pkgbuild_path = os.environ.get("PKGBUILD")
@@ -23,6 +23,7 @@ with open(pkgbuild_path, "r") as file:
             pkgver = line.split("=")[1].strip()
         if asset_name in line and repo_name in line:
             name = asset_name + line.split(asset_name)[1].strip().split('"')[0].strip()
+            break
 
 # 发送请求获取 GitHub API 数据
 response = requests.get(
@@ -46,35 +47,31 @@ if data["tag_name"].startswith("v"):
 else:
     new_pkgver = data["tag_name"].replace("-", "_")
 
-# 更新版本号和 PKGREL
-if new_pkgver != pkgver:
+# 更新版本号和 pkgver
+if new_pkgver not in pkgver:
     pkgver = new_pkgver
     version = pkgver
-    PKGREL = 1
-    UPDATE = 0
+    pkgrel = 1
+    update = 0
 
 # 下载并验证文件
 for asset in data["assets"]:
     if asset_name in asset["name"]:
-        if asset["name"] != name:
+        if asset["name"] not in name:
             name = asset["name"]
             version = name
-            UPDATE = 0
+            update = 0
         try:
-            response = requests.get(asset["browser_download_url"], timeout=10)
+            response = requests.get(asset["browser_download_url"], timeout=60)
         except requests.exceptions.RequestException:
             raise ValueError("Failed to download file")
         content = response.content
         sha256_hash = hashlib.sha256()
         sha256_hash.update(content)
         digest = sha256_hash.hexdigest()
-        if digest != asset["sha256"]:
-            raise ValueError("Downloaded file hash does not match the expected hash")
-        with open(os.path.join(os.path.dirname(pkgbuild_path), "file"), "wb") as file:
-            file.write(content)
 
 # 更新 PKGBUILD 文件和版本号文件
-if UPDATE == 0:
+if update == 0:
     lines = []
     with open(pkgbuild_path, "r") as file:
         for line in file:
@@ -83,17 +80,18 @@ if UPDATE == 0:
             if asset_name in line and repo_name in line:
                 line = "    " + line.split(asset_name)[0].strip() + name + '"' + "\n"
             if "pkgrel=" in line:
-                if PKGREL == 1:
+                if pkgrel == 1:
                     line = "pkgrel=1" + "\n"
                 else:
                     match = re.search(r"\d+", line)
                     number = int(match.group())
                     line = "pkgrel=" + str(number + 1) + "\n"
-            if "sha256" in line:
-                lines.append('    "' + digest + '"' + "\n")
-            else:
-                lines.append(line)
+            lines.append(line)
     with open(pkgbuild_path, "w") as file:
+        for i, line in enumerate(lines):
+            if "sha256" in line:
+                lines[i + 1] = '    "' + digest + '"' + "\n"
+                break
         file.writelines(lines)
     with open(os.path.join(os.path.dirname(pkgbuild_path), "version.txt"), "w") as file:
         file.write(version)
--- py/source.py
+++ py/source.py
@@ -1,33 +1,31 @@
 import hashlib
 import os
-import re
-import requests as req
+import requests
 
 # 获取环境变量
 pkgbuild_path = os.environ.get("PKGBUILD")
-repo = os.environ.get("REPO")
-latest = os.environ.get("LATEST")
+repo_name = os.environ.get("REPO")
+latest_version = os.environ.get("LATEST")
 
 # 初始化变量
 update = 1
-pkg_rel = 0
+pkgrel = 0
 pkgver = None
 new_pkgver = None
 tag_name = None
 sha256_hash = None
 
-# 检查 PKGBUILD 文件是否存在
+# 检查 PKGBUILD 文件
 if os.path.exists(pkgbuild_path):
     with open(pkgbuild_path, "r", encoding="utf-8") as file:
         for line in file:
             # 查找 pkgver
             if "pkgver=" in line:
-                pkgver = re.search(r"pkgver=(.*)", line).group(1).strip()
+                pkgver = line.split("=")[1].strip()
 
 # 发起 API 请求
-response = req.get(
-    f"https://api.github.com/repos/{repo}/releases/{latest}",
-    headers={"User-Agent": "Mozilla/5.0"},
+response = requests.get(
+    "https://api.github.com/repos/" + repo_name + "/releases/" + str(latest_version)
 )
 
 # 检查 API 响应状态码
@@ -39,21 +37,16 @@ if response.status_code == 200:
             new_pkgver = tag_name.replace("-", "_")[1:]
         else:
             new_pkgver = tag_name.replace("-", "_")
-        if new_pkgver != pkgver:
+        if new_pkgver not in pkgver:
             pkgver = new_pkgver
-            pkg_rel = 1
+            pkgrel = 1
             update = 0
-            response = req.get(
-                data["zipball_url"], headers={"User-Agent": "Mozilla/5.0"}
-            )
+            response = requests.get(data["zipball_url"])
             if response.status_code == 200:
-                file_path = os.path.join(os.path.dirname(pkgbuild_path), "file")
-                with open(file_path, "wb") as file:
-                    file.write(response.content)
-                    sha256_hash = hashlib.sha256()
-                    with open(file_path, "rb") as file:
-                        for chunk in iter(lambda: file.read(4096), b""):
-                            sha256_hash.update(chunk)
+                content = response.content
+                sha256_hash = hashlib.sha256()
+                sha256_hash.update(content)
+                digest = sha256_hash.hexdigest()
 
 # 更新 PKGBUILD 文件
 if update == 0:
@@ -61,17 +54,17 @@ if update == 0:
     with open(pkgbuild_path, "r", encoding="utf-8") as file:
         for line in file:
             if "pkgver=" in line:
-                line = f"pkgver={pkgver}\n"
+                line = "pkgver=" + pkgver + "\n"
             if "pkgrel=" in line:
-                if pkg_rel == 1:
-                    line = "pkgrel=1\n"
+                if pkgrel == 1:
+                    line = "pkgrel=1" + "\n"
             lines.append(line)
-    with open(pkgbuild_path, "w", encoding="utf-8") as file:
-        for line, next_line in zip(lines, lines[1:] + [""]):
+    with open(pkgbuild_path, "w") as file:
+        for i, line in enumerate(lines):
             if "sha256" in line:
-                next_line = f'    "{sha256_hash.hexdigest()}"\n'
-            file.write(line)
-            file.write(next_line)
+                lines[i + 1] = '    "' + digest + '"' + "\n"
+                break
+        file.writelines(lines)
     name = data["tag_name"]
     version_file_path = os.path.join(os.path.dirname(pkgbuild_path), "version.txt")
     with open(version_file_path, "w", encoding="utf-8") as file:
